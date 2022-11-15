3d3a9bda13bbe2c3d90aee4f76ead7301904944f
aparnapoola <118242262+aparnapoola@users.noreply.github.com>
2022-11-15 10:26:13 +0530

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+var a=[1,4,3,5,6,7]
+var b=[2,3,5,9,10,4]
+for(let i=0;i<=a.length;i++){
+for(let j=0;j<b.lengtg;j++){
+if(a[i]==b[i]){
+  console.log(a[i])
+}
+}
+else
+console.log("not intersection ")
