70cdc10b5799c6396548acb08a864de7d1c0d628
aparnapoola <118242262+aparnapoola@users.noreply.github.com>
2022-11-30 18:53:10 +0530

    Add files via upload

---
+++ index (10).js
@@ -0,0 +1,11 @@
+const num=025468
+const str=num.toString()
+const result=[str[0]]
+for(x=1;x<str.length;x++){
+  if((str[x-1]%2==0) && (str[x]%2==0)){
+  result.push("-",str[x])
+  }
+  else
+  result.push(str[x])
+ }
+console.log(result.join(''))
